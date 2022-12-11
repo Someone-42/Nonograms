f4d8fb78c400bfbaabe9086781c586a4f2dadfb9
Mortell560 <mortell560@gmail.com>
2022-12-11 02:32:07 +0100

    insecticide does wonders to code

--- src/UnsafeStack.py
+++ src/UnsafeStack.py
@@ -2,22 +2,22 @@
 
 class UnsafeStack:
     def __init__(self, n):
-        self.stack = []
-        self.pointer = 0
+        self.stack = [None for _ in range(n)]
+        self.pointer = -1
         self.capacity = n
         self.maxUnPop = 0 # We cannot unpop element from the get-go
 
     def push(self, value):
         """Adds an element to the stack"""
         if self.pointer < self.capacity:
-            self.stack[self.pointer] = value
             self.pointer += 1
+            self.stack[self.pointer] = value
         else:
             raise Exception("Stack overflow")
     
     def pop(self):
         """Returns the last element of the stack and moves the pointer"""
-        if self.pointer > 0:
+        if self.pointer >= 0:
             val = self.stack[self.pointer]
             self.pointer -= 1
             self.maxUnPop += 1
@@ -27,12 +27,27 @@ class UnsafeStack:
 
     def isEmpty(self):
         """Returns True if the stack is empty, False otherwise"""
-        return self.pointer == 0
+        return self.pointer == -1
 
     def UnPop(self):
         """Returns the previous element of the stack"""
-        if self.pointer < self.maxUnpop:
+        if self.pointer < self.maxUnPop:
             self.pointer += 1
             return self.stack[self.pointer]
         else:
             raise Exception("Cannot unpop")
+
+
+if __name__ == "__main__":
+    s = UnsafeStack(10)
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    print(s.pop())
+    print(s.pop())
+    #print(s.pop())
+    #print(s.pop()) # Should raise an exception
+    s.push(4)
+    print(s.pointer)
+    print(s.stack)
+    print(s.UnPop())
