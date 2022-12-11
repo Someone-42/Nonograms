980159f76bc39e00c23c95cd57f4ac1191e12e56
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2022-12-11 13:09:45 +0100

    Board now follows interface definition

--- src/Board.py
+++ src/Board.py
@@ -2,24 +2,9 @@ import numpy as np
 
 class Board:
     __slots__ = (
-        "size",         # Tuple : (x, y)
-        "constraints",  # Array of size (x + y), x first elements are the list constraints for columns
-        "grid"         # 2D boolean array of size x by y, containing either True or False
+        "grid"         # numpy 2D array<int> of size x by y, where values represent colors
         )
     def __init__(self, size, constraints):
         self.size = size
         self.constraints = constraints
-        self.grid = np.zeros((size[0], size[1])) > 0
-
-    @staticmethod
-    def from_file(path):
-        f = open(path, 'r')
-        values = [[int(c) for c in l.strip().split(' ')] for l in f.readlines()]
-        f.close()
-        return Board(tuple(values[0]), values[1:])
-
-    def display_console(self):
-        for l in self.grid:
-            for c in l:
-                print('#' if c else ' ', end = " ")
-            print()
+        self.grid = np.zeros((size[0], size[1]))
