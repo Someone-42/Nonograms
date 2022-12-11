56924aa09d4c172f5dfbaa781aa480cf26bd6f85
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2022-12-11 14:03:54 +0100

    Utils display_console_board() method

--- src/Utils.py
+++ src/Utils.py
@@ -1,2 +1,15 @@
+from Board import Board
+
+__CONSOLE_COLOR_PALETTE = ['#', 'O', '=', '+', '@']
+
 def copy_2d_list(l):
     return [s[:] for s in l]
+
+def display_console_board(board: Board):
+    for l in board.grid:
+        for c in l:
+            if c == 0:
+                print(' ', end=' ')
+                continue
+            print(__CONSOLE_COLOR_PALETTE[(c - 1) % len(__CONSOLE_COLOR_PALETTE)], end=' ')
+        print()
