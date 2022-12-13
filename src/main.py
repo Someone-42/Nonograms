1415fbd5f00ea8f5629335c1a6bdbadfe0cfb0b2
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2022-12-13 18:37:17 +0100

    Changed path for levels

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from Level import Level
 from UI import UI
 
 game = Game(UI())
-level = Level.from_file("src/TestBoard.lvl")
+level = Level.from_file("src/Levels/TestBoard.lvl")
 game.load_level(level)
 game.run()
 
